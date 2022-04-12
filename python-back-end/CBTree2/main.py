4928a086f10eafb044c36940a394e505f67f76ff
Jordan Christensen <thebigjc@gmail.com>
2022-04-12 14:49:01 +0000

    reordered imports per pylint

--- python-back-end/CBTree2/main.py
+++ python-back-end/CBTree2/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import auth, user
 from fastapi.middleware.cors import CORSMiddleware
+from routers import auth, user
 
 app = FastAPI()
 app.add_middleware(
